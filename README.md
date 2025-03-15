3066a84b7e3088b73454ff633fef7c478fb71f66
Seyhmus Akaslan <nalsakas@gmail.com>
2025-03-15 09:19:06 +0100

    initial

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # apartmentrenter
-Find minimum distance a renter would walk to reach any given target
+Find minimum distance a renter would walk to reach any given target. In other words
+minimize max walking distance for any given target.
+
+## Problem statement:
+For a a given blocks of apartments, residents needs to walk to "gmy", "school" and "store" on foot.
+If you are lucky resident all that targets can be found in the same block.
+You as a renter need to choose one of a given blocks, such that max walking distance
+to any target(gym, school or store) will be min among all
+
+## Solution:
+- Loop through all apartment blocks to calculate list of min distances one should walk for each target.
+- Keeps only max walking distance in result list, discard others
+- Loop through result list to find min walking distance block
+- Rent apartment from that block
---
+++ main.py
@@ -0,0 +1,67 @@
+import math
+
+blocks = [
+    {
+        "gym": False,
+        "school": True,
+        "store": False,
+    },
+    {
+        "gym": True,
+        "school": False,
+        "store": False,
+    },
+    {
+        "gym": True,
+        "school": True,
+        "store": False,
+    },
+    {
+        "gym": False,
+        "school": True,
+        "store": False,
+    },
+    {
+        "gym": False,
+        "school": True,
+        "store": True,
+    },
+]
+
+reqs = ["gym", "school", "store"]
+
+# Calculate distance of each reqs for all blocks
+# i = current block
+res = [None] * len(blocks)
+for i in range(len(blocks)):
+
+    # Calculate nearest reqs array for each block
+    lengths = [math.inf] * len(reqs)
+
+    # search reqs[j] in all blocks for blocks[i]
+    # j = current reqs
+    for j in range(len(reqs)):
+
+        # loop over blocks to calculate distance for current j and i.
+        k = 0
+        while k < len(blocks):
+            if blocks[k].get(reqs[j]):     # can't put a function into an [index]. So use get()
+                if abs(i -k) < lengths[j]:
+                    lengths[j] = abs(i - k)
+            k += 1
+    # save result in res[i]
+    res[i] = lengths
+
+# select max distance for any reqs member for all blocks
+# convert list[list[int]] --> list[int]
+# selected member chooses max walk distance for any given block
+res = list(map(lambda x: max(x), res))
+
+# Minimum of the walk distance? Which block? How long?
+smallest = [math.inf, math.inf]
+for i in range(len(res)):
+    if res[i] < smallest[0]: # min walk distance?
+        smallest[0] = res[i]
+        smallest[1] = i      # which block?  
+
+print(f"Block {smallest[1]}, max walk distance {smallest[0]}")
